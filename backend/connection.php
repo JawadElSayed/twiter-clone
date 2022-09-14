81417c4335582e77317a205fa5b0867334648e06
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-14 22:35:20 +0300

    checking if username exist

--- backend/connection.php
+++ backend/connection.php
@@ -9,6 +9,6 @@ $db_user = "root";
 $db_pass = null;
 $db_name = "twitter";
 
-$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+$twitter = new mysqli($host, $db_user, $db_pass, $db_name);
 
 ?>
--- backend/sign_up.php
+++ backend/sign_up.php
@@ -1,5 +1,24 @@
 <?php
 
+include("connection.php");
+
+// inputs
+$name = $_POST["name"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$dob = $_POST["dob"];
+$join_date = $_POST["join_date"];
+$response = [];
+
+// checking username if exist
+
+$select = mysqli_query($twitter, "SELECT username FROM users WHERE username = '$username'");
+if(mysqli_num_rows($select)) {
+    $response["sign_up"] = false;
+    exit($json = json_encode($response));
+}
+
 
 
 
