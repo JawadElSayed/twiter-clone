3170cd46726cdb3869067a0008d190485acef621
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-15 01:28:14 +0300

    add insert part

--- backend/sign_up.php
+++ backend/sign_up.php
@@ -7,12 +7,11 @@ $name = $_POST["name"];
 $username = $_POST["username"];
 $email = $_POST["email"];
 $password = $_POST["password"];
-$dob = $_POST["dob"];
-$join_date = $_POST["join_date"];
+$dob = DateTime::createFromFormat('d/m/Y', $_POST["dob"])->format('Y/m/d');
+$join_date = DateTime::createFromFormat('d/m/Y', $_POST["join_date"])->format('Y/m/d');
 $response = [];
 
 // checking username if exist
-
 $select = mysqli_query($twitter, "SELECT username FROM users WHERE username = '$username'");
 if(mysqli_num_rows($select)) {
     $response["sign_up"] = false;
@@ -28,5 +27,13 @@ if(mysqli_num_rows($select)) {
     exit($json = json_encode($response));
 }
 
+// insert data
+$add = $twitter->prepare("INSERT INTO users(username, name, email, password, dob, join_date) VALUE (?, ?, ?, ?, ?, ?)");
+$add->bind_param("ssssss", $username, $name, $email, $password, $dob, $join_date);
+$add->execute();
+
+$response["sign_up"] = true;
+
+echo json_encode($response);
 
 ?>
