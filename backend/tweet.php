cf48e957d740788b457a13f0198405a88f753da8
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 01:27:43 +0300

    adding tweet

--- backend/tweet.php
+++ backend/tweet.php
@@ -1,16 +1,16 @@
 <?php
 
-include("connection");
+include("connection.php");
 
 // inputs
 $user_id = $_POST["user_id"];
 $tweet = $_POST["tweet"];
-$tweet_time = DateTime::createFromFormat('d/m/Y h:m:s', $_POST["tweet_time"])->format('Y/m/d h:m:s');
+$tweet_time = DateTime::createFromFormat('j/n/Y G:i:s', $_POST["tweet_time"])->format('Y/m/d H:i:s');
 $image = $_POST["image"];
 
 // add tweet
 $query = $twitter->prepare("INSERT INTO tweets(tweet, users_id, tweet_time) VALUE (?, ?, ?)");
-$query->bind_param("sss", $tweet, $users_id, $tweet_time);
+$query->bind_param("sss", $tweet, $user_id, $tweet_time);
 $query->execute();
 
 $response = [];
