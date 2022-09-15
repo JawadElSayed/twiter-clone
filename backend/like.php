2b77f7d741912f9ec76d8c6a7d6b9fdac2fd6ca3
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 02:48:46 +0300

    adding like to tweet

---
+++ backend/like.php
@@ -0,0 +1,19 @@
+<?php
+
+include("connection.php");
+
+// inputs
+$user_id = $_POST["user_id"];
+$tweet_id = $_POST["tweet_id"];
+
+// add like
+$query = $twitter->prepare("INSERT INTO likes(users_id, tweets_id) VALUE (?, ?)");
+$query->bind_param("ss",$user_id, $tweet_id);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
