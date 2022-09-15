26435d10769fa5e14f6c1d33374b49c893f6ffb5
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 01:41:27 +0300

    adding image to tweet

--- backend/tweet.php
+++ backend/tweet.php
@@ -8,22 +8,21 @@ $tweet = $_POST["tweet"];
 $tweet_time = DateTime::createFromFormat('j/n/Y G:i:s', $_POST["tweet_time"])->format('Y/m/d H:i:s');
 $image = $_POST["image"];
 
-// add tweet
-$query = $twitter->prepare("INSERT INTO tweets(tweet, users_id, tweet_time) VALUE (?, ?, ?)");
-$query->bind_param("sss", $tweet, $user_id, $tweet_time);
-$query->execute();
-
-// adding image
+// decoding and saving image
 $image = explode( ',', $image );
 $image = $image[1];
 $data = base64_decode($image);
 $file = "images/" . uniqid() . '.png';
 $success = file_put_contents($file, $data);
 
+// add tweet
+$query = $twitter->prepare("INSERT INTO tweets(tweet, image, users_id, tweet_time) VALUE (?, ?, ?, ?)");
+$query->bind_param("ssss", $tweet, $file, $user_id, $tweet_time);
+$query->execute();
+
 $response = [];
 $response["success"] = true;
 
 echo json_encode($response);
-
 
 ?>
