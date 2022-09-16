2584ff1d530f958be8a333c133d5ea546e78e273
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-16 03:01:53 +0300

    return count of likes for tweet

--- backend/like.php
+++ backend/like.php
@@ -11,6 +11,9 @@ $query = $twitter->prepare("INSERT INTO likes(users_id, tweets_id) VALUE (?, ?)"
 $query->bind_param("ss",$user_id, $tweet_id);
 $query->execute();
 
+// returning count likes
+
+
 $response = [];
 $response["success"] = true;
 
