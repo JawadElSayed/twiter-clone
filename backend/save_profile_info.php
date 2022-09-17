11499a250cda2aa3fd76c7236edc4e8d070d3d7f
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-17 23:15:12 +0300

    adding condition on username in save profile info api

--- backend/save_profile_info.php
+++ backend/save_profile_info.php
@@ -11,4 +11,12 @@ $profile_img = $_POST["profile_img"];
 $cover_img = $_POST["cover_img"];
 $bio = $_POST["bio"];
 
+// checking username if exist
+$select = mysqli_query($twitter, "SELECT username FROM users WHERE username = '$username' AND NOT id = '$user_id'");
+if(mysqli_num_rows($select)) {
+    $response["success"] = false;
+    $response["error"] = "username";
+    exit($json = json_encode($response));
+}
+
 ?>
