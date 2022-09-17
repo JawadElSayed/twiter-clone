56eda90dfde20b6f8801e7322fca87fa1900ed20
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-17 23:09:14 +0300

    creating variables for save profile info api

---
+++ backend/save_profile_info.php
@@ -0,0 +1,14 @@
+<?php
+
+include("connection.php");
+include("encode_decode_image.php");
+
+// variables
+$user_id = $_POST["user_id"];
+$username = $_POST["username"];
+$name = $_POST["name"];
+$profile_img = $_POST["profile_img"];
+$cover_img = $_POST["cover_img"];
+$bio = $_POST["bio"];
+
+?>
