14d578cfb441738b9bd45bce61ae1a1314f3204c
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-17 19:27:06 +0300

    getting follow info

--- backend/follow_info.php
+++ backend/follow_info.php
@@ -3,15 +3,23 @@
 include("connection.php");
 
 // variables
-$user_id = $_POST["user_info"];
+$user_id = $_POST["user_id"];
 $response = [];
 
 // getting follow info
-$sql_following = "SELECT COUNT(follower_id)
+$sql_following = "SELECT COUNT(follower_id) as following
                     FROM `followers`
                     WHERE follower_id = '$user_id'";
-$sql_follows = "SELECT COUNT(follower_id)
+$sql_followers = "SELECT COUNT(follower_id) as followers
                 FROM `followers`
                 WHERE followed_id = '$user_id'";
+
+$following = mysqli_query($twitter,$sql_following)->fetch_object()->following;
+$followers = mysqli_query($twitter,$sql_followers)->fetch_object()->followers;
+
+$response["following"] = $following;
+$response["followers"] = $followers;
+
+echo json_encode($response);
 
 ?>
