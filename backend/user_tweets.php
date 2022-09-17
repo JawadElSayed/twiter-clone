a6fff843a72d4f3e5abf8c7e4a2e68c916f10e3f
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-17 21:10:23 +0300

    sql code for user tweets

---
+++ backend/user_tweets.php
@@ -0,0 +1,19 @@
+<?php
+
+include("connection.php");
+include("encode_decode_image.php");
+
+// variables
+$user_id = $_POST["user_id"];
+$response = [];
+
+// getting tweets data
+$sql = "SELECT tweets.id, users.name, users.username, users.profile_img, tweets.tweet, tweets.image, COUNT(likes.users_id) as likes
+        FROM `tweets`
+        INNER JOIN users ON tweets.users_id = users.id
+        LEFT JOIN likes ON tweets.id = likes.tweets_id
+        WHERE tweets.users_id = '$user_id'
+        GROUP BY tweets.id
+        ORDER BY tweets.tweet_time";
+
+?>
