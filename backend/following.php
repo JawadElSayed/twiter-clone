e1c6faf34cf38f5bf21cd2edbca14b14e323c701
jawad alsayed <jawadalsayed@jawads-MacBook-Pro.local>
2022-09-18 11:24:58 +0300

    adding unfollow case to following api

--- backend/following.php
+++ backend/following.php
@@ -13,4 +13,14 @@ $slq = "SELECT *
         WHERE follower_id = '$follower_id' AND followed_id = '$followed_id'";
 $check = mysqli_query($twitter, $slq);
 
+// add follow or unfollow
+if (mysqli_num_rows($check)){
+    $unfollow_sql = "DELETE FROM followers WHERE follower_id = '$follower_id' and followed_id = '$followed_id'";
+    $add = $twitter->prepare($unfollow_sql);
+    $add->execute();
+    $response["success"] = "unfollow";
+    echo json_encode($response);
+    exit();
+}
+
 ?>
